574314558e075e5b4b5564f406ac3dd531da0648
BluGenes-source <hb1451@yahoo.com>
2022-11-22 17:26:30 -0600

    Typo fix

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -15,7 +15,7 @@ MainComponent::MainComponent()
 		false,                    // is for saving,
 		{"*.csv"},                // browser wildcard suffix,
 		{},                       // enforced suffix,
-		"Select CSV file to open or drop a CSV file here"));  // text when nothing selected
+		"Select CSV file to open - or - drop a file here"));  // text when nothing selected
 	addAndMakeVisible(fileComp.get());
 	fileComp->addListener(this);
 	fileComp->setDefaultBrowseTarget(File::getSpecialLocation(File::userDocumentsDirectory));
